c02c2f7d8691e8b49611b83b416db0140783511b
raiyan22 <raiyanislam058@gmail.com>
2023-08-27 12:06:20 +0600

    added top section

---
+++ app/top-section.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import ProfileImage from '../public/propic.png'
+
+export default function TopSection() {
+    return (
+        <div className="my-16 sm:my-24 md:my-32">
+            <div className="flex items-center justify-end">
+                <div className="mr-4 sm:mr-8 text-right">
+                    <span className="text-2xl sm:text-3xl md:text-4xl text-slate-300 font-bold tracking-wide">
+                        Md. Raiyanul Islam
+                    </span>
+                    <p className="text-sm sm:text-base text-slate-500">
+                        Fresh graduate looking for Software Engineer role
+                    </p>
+
+                </div>
+                <div>
+                    <Image src={ProfileImage} alt="profile-image" quality={100}/>
+                </div>
+            </div>
+        </div>
+        
+    )
+}
