a604384ec75851f56b9c4232a726cbbf101eba23
raiyan22 <raiyanislam058@gmail.com>
2023-08-27 12:58:02 +0600

    !info section done

---
+++ app/information-section.tsx
@@ -0,0 +1,52 @@
+import SectionTitleHeader from "./section-title-header"
+
+export default function InfoSection({ title }: { title: String }) {
+    const information = [
+        {
+            "label": "name",
+            "value": "Md. Raiyanul Islam"
+        },
+        {
+            "label": "dob",
+            "value": "29 December, 1999"
+        },
+        {
+            "label": "location",
+            "value": "Dhaka, Bangladesh"
+        },
+        {
+            "label": "languages",
+            "value": "English, Bangla, Hindi"
+        },
+        {
+            "label": "experience",
+            "value": "n/a"
+        },  
+        {
+            "label": "status",
+            "value": "Always exploring new ideas"
+        }
+    ]
+    return (
+        <div className="mb-12">
+            <SectionTitleHeader title="Information" />
+            <div className="text-gray-400 cursor-default">
+                {information.map((item) => (
+                    <div>
+                        <div className="flex flex-row mb-4">
+                            <div className="basis-1/3">
+                                <p>
+                                    <span className="text-blue-600 font-black">_</span>
+                                    {item.label}
+                                </p>
+                            </div>
+                            <div className="basis-2/3">
+                                <p>{item.value}</p>
+                            </div>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import TopSection from './top-section'
 import DescriptionSection from './desc-section'
+import InfoSection from './information-section'
 
 export default function Home() {
+  const infoString = "Information"
   return (
     <main className="max-w-3xl mx-auto p-4 sm:p-3 md:p-2 font-mono">
-      <TopSection/>
+      <TopSection />
       <DescriptionSection/>
+      <InfoSection title={infoString}/>
     </main>
   )
 }
