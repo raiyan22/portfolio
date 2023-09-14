a1c4bbf3680494b961a8905b8c9854f7caaef722
raiyan22 <raiyanislam058@gmail.com>
2023-09-14 21:10:10 +0600

    updated cv

--- app/desc-section.tsx
+++ app/desc-section.tsx
@@ -15,7 +15,7 @@ export default function DescriptionSection() {
             </p>
             <p className="text-gray-400 mt-5">
                 {/* Here is my <a href="./Md_Raiyanul_Islam_CV.pdf" download="Md_Raiyanul_Islam_CV.pdf"> <span className="text-lg text-sky-500 font-bold">CV</span>. </a> */}
-                Here is my updated <a href="https://drive.google.com/file/d/1Cm_766QSeIykXCLsyAhelbX1ipHbZHoc/view?usp=sharing" download="Md_Raiyanul_Islam_CV.pdf"> <span className=" animate-pulse text-xl text-sky-500 font-bold ">resume</span>. </a>
+                Here is my updated <a href="https://drive.google.com/file/d/1m0WKFkLzY6mPwTva3UhWGXK1pfzqns_K/view?usp=sharing" download="Md_Raiyanul_Islam_CV.pdf"> <span className=" animate-pulse text-xl text-sky-500 font-bold ">resume</span>. </a>
             </p>
         </div>
     )
