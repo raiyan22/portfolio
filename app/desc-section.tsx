22f061f2f5f009b9e56ec78abb942a41d3069c25
raiyan22 <raiyanislam058@gmail.com>
2023-09-17 19:37:06 +0600

    updated cv

--- app/desc-section.tsx
+++ app/desc-section.tsx
@@ -1,4 +1,7 @@
 export default function DescriptionSection() {
+
+    const cv_link = "https://drive.google.com/file/d/1L08e0peSZUpGHBFVOkZwoVn_J_uKkOhm/view?usp=sharing"
+
     return (
         <div className="mb-12">
             <p className="text-gray-400">
@@ -15,7 +18,8 @@ export default function DescriptionSection() {
             </p>
             <p className="text-gray-400 mt-5">
                 {/* Here is my <a href="./Md_Raiyanul_Islam_CV.pdf" download="Md_Raiyanul_Islam_CV.pdf"> <span className="text-lg text-sky-500 font-bold">CV</span>. </a> */}
-                Here is my updated <a href="https://drive.google.com/file/d/1m0WKFkLzY6mPwTva3UhWGXK1pfzqns_K/view?usp=sharing" download="Md_Raiyanul_Islam_CV.pdf"> <span className=" animate-pulse text-xl text-sky-500 font-bold ">resume</span>. </a>
+                Here is my updated <a target="_blank" href={cv_link}> <span className=" animate-pulse text-xl text-sky-500 font-bold ">resume</span>. </a>
+                {/* <a href={cv_link}>  </a> */}
             </p>
         </div>
     )
